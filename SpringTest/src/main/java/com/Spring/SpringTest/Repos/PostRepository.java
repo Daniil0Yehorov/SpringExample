8fe6cd61cf6ccce0f5a07ba84d5d07aba9c6b25e
Daniil0Yehorov <114640212+Daniil0Yehorov@users.noreply.github.com>
2024-03-05 22:43:51 +0100

    Update PostRepository.java

--- SpringTest/src/main/java/com/Spring/SpringTest/Repos/PostRepository.java
+++ SpringTest/src/main/java/com/Spring/SpringTest/Repos/PostRepository.java
@@ -2,7 +2,7 @@ package com.Spring.SpringTest.Repos;
 
 import com.Spring.SpringTest.models.Post;
 import org.springframework.data.repository.CrudRepository;
-//все функции в крудрепозитории есть получение и т.д
+//all functions in the crudrepository are receiving, etc.
 public interface PostRepository extends CrudRepository<Post,Long> {
 
 }
