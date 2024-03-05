d93154cad0941cbb1fbce403f1a38b7c493cf5bb
Daniil0Yehorov <114640212+Daniil0Yehorov@users.noreply.github.com>
2024-03-05 22:38:10 +0100

    Create User.java

---
+++ SpringTest/src/main/java/com/Spring/SpringTest/models/User.java
@@ -0,0 +1,39 @@
+package com.Spring.SpringTest.models;
+
+import jakarta.persistence.*;
+@Entity //анотация сущности
+public class User {
+    @Id //анотация айди
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,unique = true)
+    private long id;
+    @Column(nullable = false,unique = true)
+    private String login;
+    @Column(nullable = false)
+    private String password;
+    public User (){}
+    public User (String login, String password){this.login=login;this.password=password;}
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
